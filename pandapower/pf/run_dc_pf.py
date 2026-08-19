Fix baseMVA scaling for DC power and current
Convert DC loads from MW to per-unit before solving and apply baseMVA scaling when calculating DC branch currents.

pandapower/pf/run_dc_pf.py
@@ -95,8 +95,8 @@ def _run_dc_pf(ppci, recycle: dict | bool = False):
     # compute complex bus power injections [generation - load]
     # adjusted for phase shifters and real shunts
     Pbus = np.real(makeSbus(baseMVA, bus, gen)) - bus[:, GS] / baseMVA
-    # add dc nodes respecting loads
-    Pbus = np.concatenate([Pbus, -bus_dc[:, DC_PD]])
+    # DC_PD is stored in MW, while Pbus contains per-unit power injections.
+    Pbus = np.concatenate([Pbus, -bus_dc[:, DC_PD] / baseMVA])
     # select VSCs with mode DC p and not mode AC slack
     vsc_with_p = vsc[(vsc[:, VSC_MODE_DC] == VSC_MODE_DC_P) & (vsc[:, VSC_MODE_AC] != VSC_MODE_AC_SL)]
     ac_bus = vsc_with_p[:, VSC_BUS].astype(int64)
@@ -138,9 +138,9 @@ def _run_dc_pf(ppci, recycle: dict | bool = False):
     # the DC branches: DC_PF and DC_PT from the matrix
     branch_dc[:, DC_PF] = (Bf * Va + Pfinj)[branch.shape[0] + vsc.shape[0]:] * baseMVA
     branch_dc[:, DC_PT] = -branch_dc[:, DC_PF]
-    # set the currents as the nominal power (the currents are calculated in results_branch)
-    branch_dc[:, DC_IF] = branch_dc[:, DC_PF]
-    branch_dc[:, DC_IT] = branch_dc[:, DC_PT]
+    # With the linear DC voltage fixed at 1 pu, branch current in pu equals branch power in pu.
+    branch_dc[:, DC_IF] = branch_dc[:, DC_PF] / baseMVA
+    branch_dc[:, DC_IT] = branch_dc[:, DC_PT] / baseMVA
     # on AC nodes considered for the angle
     bus[:, VA] = Va[:bus.shape[0]] * (180. / pi)
 

pandapower/pypower/newtonpf.py
@@ -178,7 +178,8 @@ def newtonpf(Ybus, Sbus, V0, ref, pv, pq, ppci, options, makeYbus=None):
     relevant_bus_dc = flatnonzero(bus_dc[:, DC_BUS_TYPE] != DC_NONE)
     num_bus_dc = len(relevant_bus_dc)
     any_branch_dc = num_branch_dc > 0
-    P_dc = -bus_dc[relevant_bus_dc, DC_PD] # load is negative here
+    # DC_PD is stored in MW, while the Newton mismatch equations use per-unit power.
+    P_dc = -bus_dc[relevant_bus_dc, DC_PD] / baseMVA  # load is negative here
     p_set_point_index = vsc_controllable & (vsc_mode_dc == VSC_MODE_DC_P) & (vsc_mode_ac != VSC_MODE_AC_SL)
     # P_dc[vsc[p_set_point_index, VSC_BUS_DC].astype(np.int64)] = -vsc_value_dc[vsc_mode_dc == 1]  # todo sum by group
     # todo vsc
@@ -647,7 +648,8 @@ def newtonpf(Ybus, Sbus, V0, ref, pv, pq, ppci, options, makeYbus=None):
 
         # Pbus_dc = V_dc * (Ybus_hvdc + Ybus_vsc_dc).dot(V_dc)
         Pbus_dc = V_dc * Ybus_hvdc.dot(V_dc)
-        bus_dc[relevant_bus_dc, DC_PD] = Pbus_dc
+        # Pbus_dc is calculated in pu, while DC_PD is defined and exported in MW.
+        bus_dc[relevant_bus_dc, DC_PD] = Pbus_dc * baseMVA
 
         # update branch flows
         i_hvdc_f = Yf_hvdc.dot(V_dc)

pandapower/test/loadflow/test_facts_b2b_vsc.py
@@ -80,8 +80,8 @@ def test_hvdc_interconnect_with_dmr():
     assert np.isclose(net.res_line_dc.loc[dmr, 'i_ka'], 0.133, atol=0.001)
 
 
-def test_source_dc():
-    net = create_empty_network()
+def _create_source_dc_test_network(sn_mva):
+    net = create_empty_network(sn_mva=sn_mva)
     create_bus(net, 380)
     create_bus(net, 380)
     create_ext_grid(net, bus=0, vm_pu=1.0)
@@ -94,9 +94,34 @@ def test_source_dc():
 
     create_source_dc(net, bus_dc=0, vm_pu=.5)
     create_load_dc(net, bus_dc=1, p_dc_mw=10)
+    return net
 
-    runpp(net)
-    pass
+
+def test_source_dc_results_independent_of_sn_mva():
+    nets = [_create_source_dc_test_network(sn_mva) for sn_mva in (1, 10)]
+    for net in nets:
+        runpp(net)
+
+        # The source result is derived from the solved DC bus power and must be reported in MW.
+        np.testing.assert_allclose(
+            net.res_source_dc.at[0, "p_dc_mw"],
+            net.res_line_dc.at[0, "p_from_mw"],
+            rtol=1e-9,
+            atol=1e-9,
+        )
+        np.testing.assert_allclose(
+            net.res_line_dc.at[0, "p_to_mw"],
+            -net.load_dc["p_dc_mw"].iat[0] * net.load_dc["scaling"].iat[0],
+            rtol=1e-9,
+            atol=1e-8,
+        )
+
+    for table, columns in {
+        "res_bus_dc": ["vm_pu", "p_mw"],
+        "res_line_dc": ["p_from_mw", "p_to_mw", "pl_mw", "i_ka"],
+        "res_source_dc": ["p_dc_mw"],
+    }.items():
+        np.testing.assert_allclose(nets[0][table][columns], nets[1][table][columns], rtol=1e-9, atol=1e-9)
 
 
 @pytest.mark.xfail

pandapower/test/loadflow/test_rundcpp.py
@@ -12,7 +12,7 @@ import pytest
 from pandapower.auxiliary import _check_connectivity, _add_ppc_options, LoadflowNotConverged
 from pandapower.create import (create_empty_network, create_bus, create_transformer, create_transformer3w, create_load,
                                create_xward, create_switch, create_ext_grid, create_line_from_parameters, create_bus_dc,
-                               create_vsc, create_line_dc_from_parameters)
+                               create_vsc, create_line_dc_from_parameters, create_load_dc)
 from pandapower.networks.power_system_test_cases import case4gs, case118
 from pandapower.pd2ppc import _pd2ppc
 from pandapower.run import rundcpp, runpp
@@ -82,6 +82,42 @@ def test_test_sn_mva():
             assert_net_equal(net1, net2, exclude_elms=["sn_mva"])
         except:
             raise UserWarning("Result difference due to sn_mva after adding %s" % net1.last_added_case)
+
+
+def _create_load_dc_test_network(sn_mva):
+    net = create_empty_network(sn_mva=sn_mva)
+
+    ac_slack_bus = create_bus(net, vn_kv=110)
+    ac_vsc_bus = create_bus(net, vn_kv=110)
+    create_ext_grid(net, ac_slack_bus)
+    create_line_from_parameters(net, ac_slack_bus, ac_vsc_bus, length_km=1, r_ohm_per_km=0.05,
+                                x_ohm_per_km=0.1, c_nf_per_km=0, max_i_ka=1)
+
+    dc_vsc_bus = create_bus_dc(net, vn_kv=110)
+    dc_load_bus = create_bus_dc(net, vn_kv=110)
+    create_line_dc_from_parameters(net, dc_vsc_bus, dc_load_bus, length_km=1, r_ohm_per_km=0.2,
+                                   max_i_ka=1)
+    create_vsc(net, ac_vsc_bus, dc_vsc_bus, r_ohm=0.1, x_ohm=5, r_dc_ohm=0.15,
+               control_mode_ac="q_mvar", control_value_ac=0,
+               control_mode_dc="vm_pu", control_value_dc=1)
+    create_load_dc(net, dc_load_bus, p_dc_mw=10)
+    return net
+
+
+@pytest.mark.parametrize("solver", [runpp, rundcpp], ids=["runpp", "rundcpp"])
+def test_load_dc_results_independent_of_sn_mva(solver):
+    nets = [_create_load_dc_test_network(sn_mva) for sn_mva in (1, 10)]
+    for net in nets:
+        solver(net)
+
+    # Changing the calculation base must not change results expressed in physical units.
+    result_columns = {
+        "res_bus_dc": ["vm_pu", "p_mw"],
+        "res_line_dc": ["p_from_mw", "p_to_mw", "pl_mw", "i_ka"],
+        "res_vsc": ["p_mw", "p_dc_mw"],
+    }
+    for table, columns in result_columns.items():
+        np.testing.assert_allclose(nets[0][table][columns], nets[1][table][columns], rtol=1e-7, atol=1e-9)
 
 
 def test_single_bus_network():